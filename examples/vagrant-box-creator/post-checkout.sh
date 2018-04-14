2e84aef93b3fafaee87a85b1b47cc1b632082cfb
hanovruslan <hanov.ruslan@gmail.com>
2018-04-15 00:25:54 +0300

    Fixed PWD

--- examples/vagrant-box-creator/post-checkout.sh
+++ examples/vagrant-box-creator/post-checkout.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 self_dir="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-
+export _PWD=${PWD}
 git clone git@github.com:hanovruslan/bash-dep.git "${PWD}/bash-dep"
 
 bg_hint () {
-  echo -e "\n\nrun ${PWD}/bash-dep/dep/vagrant-box-creator/src/src-external.sh \${machine_name} \${path_to_dir_with_vagrantfile} \${vagrantfile_name}\n\n"
+  echo -e "\n\nrun ${_PWD}/bash-dep/dep/vagrant-box-creator/src/src-external.sh \${machine_name} \${path_to_dir_with_vagrantfile} \${vagrantfile_name}\n\n"
 }
 
 . ./bash-dep/add.sh "${self_dir}/vagrant-box-creator-env.sh"
